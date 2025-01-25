b8b90de720a1757def45f9a17b61a3024610fdfb
Miro Gohacki <mirogohacki@gmail.com>
2025-01-25 13:34:54 -0500

    onelink assignment complete

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
