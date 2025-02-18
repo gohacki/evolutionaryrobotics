90c87d73f16271929904026fc815c0803da86ef5
Miro Gohacki <mirogohacki@gmail.com>
2025-02-18 09:34:07 -0500

    assignment 5

--- analyze.py
+++ analyze.py
@@ -7,7 +7,14 @@ with open('data/backLegSensorValues.npy', 'rb') as f:
     backLegSensorValues = numpy.load(f)
 with open('data/frontLegSensorValues.npy', 'rb') as f:
     frontLegSensorValues = numpy.load(f)
-matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=6)
-matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg")
+
+amplitude, frequency, phaseOffset = numpy.pi/4, 10, 0
+targetAngles = amplitude * numpy.sin(frequency * numpy.linspace(0, 2 * numpy.pi, 1000) + phaseOffset)
+
+matplotlib.pyplot.plot(targetAngles, label="Motor")
+
+# matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=6)
+# matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg")
+
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
