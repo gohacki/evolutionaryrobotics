d211feecb6b69e2bf7c21598cdd18afd1b7e4d96
Miro Gohacki <mirogohacki@gmail.com>
2025-03-09 10:09:15 -0400

    assignment 7

--- motor.py
+++ motor.py
@@ -7,25 +7,9 @@ import pybullet as p
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-        self.motorValues = numpy.zeros(c.ITERATIONS)
-
-        self.amplitude = c.AMPLITUDE
-        self.offset    = c.PHASEOFFSET
-
-        if self.jointName == b'Torso_BackLeg':
-            self.frequency = c.FREQUENCY / 2
-        else:
-            self.frequency = c.FREQUENCY
-
-        self.motorValues = self.amplitude * numpy.sin(
-            self.frequency * numpy.linspace(c.RANGESTART, c.RANGEEND, c.ITERATIONS) + self.offset
-        )
-
-    def Set_Value(self, robot, i):
-        targetPosition = self.motorValues[i]
+    def Set_Value(self, robot, desiredAngle):
+        targetPosition = desiredAngle
         pyrosim.Set_Motor_For_Joint(
             bodyIndex     = robot.robotID,
             jointName     = self.jointName,
@@ -33,7 +17,3 @@ class MOTOR:
             targetPosition= targetPosition,
             maxForce      = c.MAXFORCE
         )
-    
-    def Save_Values(self):
-        import numpy as np
-        np.save('data/{}_MotorValues.npy'.format(self.jointName), self.motorValues)
--- robot.py
+++ robot.py
@@ -2,6 +2,8 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 from sensor import SENSOR
 from motor import MOTOR
+from pyrosim.neuralNetwork import NEURAL_NETWORK
+
 
 class ROBOT:
     def __init__(self):
@@ -9,6 +11,7 @@ class ROBOT:
         self.sensors = {}
         self.robotID = p.loadURDF("body.urdf")
         pyrosim.Prepare_To_Simulate(self.robotID)
+        self.nn = NEURAL_NETWORK("brain.nndf")
         self.Prepare_To_Sense()
         self.Prepare_To_Act()
     
@@ -26,11 +29,24 @@ class ROBOT:
             sensor.Get_Value(i)
 
     def Act(self, i):
-        for motor in self.motors.values():
-            motor.Set_Value(self, i)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                self.nn.Print()
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName).encode("utf-8")
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                if jointName in self.motors:
+                    self.motors[jointName].Set_Value(self, desiredAngle)
+                print(neuronName, jointName, desiredAngle)
+
+
+    def Think(self):
+        self.nn.Update()
+
+
         
     def Save_Values(self):
         for sensor in self.sensors.values():
             sensor.Save_Values()
         for motor in self.motors.values():
             motor.Save_Values()
+        
